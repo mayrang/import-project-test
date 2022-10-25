8d63b2940f161ed85f875fc56035d449170f4c82
mayrang <pkss0626@naver.com>
2022-10-25 09:56:50 +0900

    스케줄 목록 일부 구현, 예약 목록 디자인 일부 수정, 예약과 스케줄 목록 모달 시작날짜순으로 표시하는 것으로 변경

--- client/src/components/ReservationPostsModal.jsx
+++ client/src/components/ReservationPostsModal.jsx
@@ -2,7 +2,9 @@ import dayjs from "dayjs";
 import React from "react";
 
 const ReservationPostsModal = ({postDate, setPostDate, user, mutate, setReservationPostsModal, setReservationAddModal}) => {
-    
+    // 각 날짜의 포스트 시작시간순으로 배열
+    const sortedPosts = postDate?.posts?.sort((a, b) => new Date(a.startTime) - new Date(b.startTime));
+    console.log(sortedPosts)
     const closePostsModal = () => {
         setPostDate({});
         setReservationPostsModal(false);
@@ -16,7 +18,7 @@ const ReservationPostsModal = ({postDate, setPostDate, user, mutate, setReservat
                 </h3>   
             </div>
             <div className="border-b px-4 py-2 flex-col text-left items-center">                  
-                {postDate?.posts?.map((post) => (
+                {sortedPosts?.map((post) => (
                     <div key={post.calendarId} className="flex border-b items-center justify-between">
                         <div className="flex items-center justify-center">
                             <div className="flex-col justify-center items-center p-2">
@@ -28,11 +30,20 @@ const ReservationPostsModal = ({postDate, setPostDate, user, mutate, setReservat
                                 <p className="font-medium text-sm">종료시간: {dayjs(post.endTime).format("DD일 HH:mm")}</p>
                             </div>
                         </div>
+                        <div className="p-2 flex-col md:flex items-center">
+                            {post.userId === user.userId && (
+                                <>
+                                <button className="p-2 border rounded bg-white border-green-500 hover:bg-green-500 hover:text-white">수정</button>
+                                <button className="mt-1 md:ml-2 p-2 border rounded bg-white border-red-500 hover:bg-red-500 hover:text-white">삭제</button>
+                                </>
+                          
+                            )}
+                        </div>
                         
                     </div>
                 ))}
             </div>
-            <div className="flex justify-between items-center w-full p-3">
+            <div className="flex justify-end items-center w-full p-3">
                 <div>
                     <button onClick={closePostsModal} className="bg-gray-200 border hover:text-white rounded p-2">닫기</button>
                 </div>
--- client/src/components/SchedulePostsModal.jsx
+++ client/src/components/SchedulePostsModal.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import dayjs from "dayjs";
 
-const SchedulePostsModal = () => {
+const SchedulePostsModal = ({postDate, setPostDate, user, mutate, setSchedulePostsModal, setScheduleAddModal, }) => {
 
+    const sortedPosts = postDate?.posts.sort((a, b) => new Date(a.startTime) - new Date(b.startTime));
+
+    const closePostsModal = () => {
+        setSchedulePostsModal(false);
+        setPostDate({});
+    };
+
+    return (
+        <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black bg-opacity-40 text-center">
+        <div className="bg-white rounded w-10/12">
+            <div className="flex justify-between items-start p-4 border-b ">
+                <h3 className="text-xl font-semibold text-gray-900">
+                    {postDate.date}일
+                </h3>   
+            </div>
+            <div className="border-b px-4 py-2 flex-col text-left items-center">                  
+                {sortedPosts?.map((post) => (
+                    <div key={post.calendarId} className="flex border-b items-center justify-between">
+                        <div className="flex items-center justify-center">
+                            <div className="flex-col justify-center items-center p-2">
+                                <p className="font-medium text-sm">일정: {post.content}</p>
+                               
+                            </div>
+                            <div className="flex-col justify-center items-center p-2">
+                                <p className="font-medium text-sm">시작시간: {dayjs(post.startTime).format("DD일 HH:mm")}</p>
+                                <p className="font-medium text-sm">종료시간: {dayjs(post.endTime).format("DD일 HH:mm")}</p>
+                            </div>
+                        </div>
+                        <div className="p-2 flex-col md:flex items-center ">
+                            {post.userId === user.userId && (
+                                <>
+                                <button className="p-2 border rounded bg-white border-green-500 hover:bg-green-500 hover:text-white">수정</button>
+                                <button className="mt-1 md:ml-2 p-2 border rounded bg-white border-red-500 hover:bg-red-500 hover:text-white">삭제</button>
+                                </>
+                          
+                            )}
+                        </div>
+                        
+                    </div>
+                ))}
+            </div>
+            <div className="flex justify-end items-center w-full p-3">
+                <div>
+                    <button onClick={closePostsModal} className="bg-gray-200 border hover:text-white rounded p-2">닫기</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
 };
 
 export default SchedulePostsModal;
