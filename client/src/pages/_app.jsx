2685823963cd481d8ead6bb9470e3790604d99c4
mayrang <pkss0626@naver.com>
2022-11-05 22:24:35 +0900

    recoil 초기 세팅

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,10 +1,14 @@
 import axios from 'axios';
 import '../styles/globals.css';
-
+import {RecoilRoot} from "recoil";
 function MyApp({ Component, pageProps }) {
   axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
   axios.defaults.withCredentials = true;
-  return <Component {...pageProps} />
+  return (
+    <RecoilRoot>
+    <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 
 export default MyApp
