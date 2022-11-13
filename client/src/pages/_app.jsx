b24b8c12ed6b913b159b0c199bf2f2b9508dc95d
mayrang <pkss0626@naver.com>
2022-11-14 01:41:21 +0900

    next-redux-wrapper lagacy 수정

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,15 +1,21 @@
 import axios from 'axios';
+import { Provider } from 'react-redux';
 import wrapper from '../redux/store';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, ...rest }) {
   axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
   axios.defaults.withCredentials = true;
-  return (
+  const {store, props} = wrapper.useWrappedStore(rest);
 
-    <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+
+    <Component {...props.pageProps} />
+    </Provider>
+    
 
   );
 }
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
